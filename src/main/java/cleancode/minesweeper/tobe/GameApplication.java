5c9cb97f8db9e34a1d62224441b8e55e3fb6262e
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-06 17:27:36 +0900

    refact : SRP

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,8 @@
+package cleancode.minesweeper.tobe;
+
+public class GameApplication {
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.run();
+    }
+}
